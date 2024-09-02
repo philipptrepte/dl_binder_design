2c3c6ef30fe72a5ac6f3392743045dabc270193b
philipptrepte <93270384+philipptrepte@users.noreply.github.com>
2024-09-02 16:08:21 +0200

    new function clean_af2results

--- af2_initial_guess/clean_af2.py
+++ af2_initial_guess/clean_af2.py
@@ -7,23 +7,6 @@ import shutil
 import subprocess
 import os
 
-#################################
-# Parse Arguments
-#################################
-
-parser = argparse.ArgumentParser()
-
-# I/O Arguments
-parser.add_argument( "-score", type=str, default="", help='The path of a file of af2-initial guess scores' )
-parser.add_argument( "-pae", type=str, default="", help='The path of a file of af2-initial guess pae values' )
-parser.add_argument( "-silent", type=str, default="af2.silent", help='The path of the af2-initial guess silent file' )
-parser.add_argument( "-checkpoint", type=str, default="", help='The path of af2-initial guess checkpoint file which will be overwritten' )
-parser.add_argument( "-reference", type=str, default="pae_description", help='Cleaning checkpoint file referencing to the missing pae (`pae_description`) or missing score file (`description`)' )
-
-args = parser.parse_args()
-
-#################################
-
 def clean_checkpoint(ids, af2checkpoint):
     """
     Remove identifiers from checkpoint file that are not in `ids`.
@@ -62,7 +45,7 @@ def clean_checkpoint(ids, af2checkpoint):
     elif missing_ids.all():
         print("All ids in checkpoint file are in score file description. \n")
 
-def clean_af2score(af2score, af2checkpoint, af2reference='pae_description'):
+def duplicates_af2score(af2score, af2checkpoint, af2reference='pae_description'):
     """
     Cleans the AF2 initial guess score file by removing duplicates found in the 'af2reference' column and removes the respective identifiers from the 'af2checkpoint' file.
 
@@ -104,12 +87,12 @@ def clean_af2score(af2score, af2checkpoint, af2reference='pae_description'):
 
     return scores_no_duplicates
 
-def clean_pae(af2pae, af2checkpoint):
+def duplicates_pae(af2pae, af2checkpoint):
     """
     Cleans the PAE file by removing duplicates and removes the respective identifiers from the af2checkpoint file.
     
     Parameters:
-    - pae_file (str): The path to the PAE file.
+    - af2pae (str): The path to the PAE file.
     - af2checkpoint (str): The path to the checkpoint file.
     - af2reference (str, optional): The reference column name in the PAE file. Defaults to 'pae_description'.
     
@@ -128,12 +111,12 @@ def clean_pae(af2pae, af2checkpoint):
     if csv.map(lambda cell: any(substring in str(cell) for substring in ['[', ']'])).any().any():
         print("Old pae file format detected \n")
         pae = pd.DataFrame()
-        for chunk in pd.read_csv(pae_file, sep = '\[ |\]|\s+\]', index_col=False, header=None, engine='python', chunksize=chunksize):
+        for chunk in pd.read_csv(af2pae, sep = '\[ |\]|\s+\]', index_col=False, header=None, engine='python', chunksize=chunksize):
             pae = pd.concat([pae, chunk])
     elif csv.map(lambda cell: any(substring in str(cell) for substring in ['tag:'])).any().any():
         print("New pae file format detected \n")
         pae = pd.DataFrame()
-        for chunk in pd.read_csv(pae_file, sep = r'pae: | tag: ', index_col=False, header=None, engine='python', usecols=[1, 2], chunksize=chunksize):
+        for chunk in pd.read_csv(af2pae, sep = r'pae: | tag: ', index_col=False, header=None, engine='python', usecols=[1, 2], chunksize=chunksize):
             pae = pd.concat([pae, chunk])
     else:
         raise ValueError("The PAE file does not contain the expected format.")
@@ -151,12 +134,15 @@ def clean_pae(af2pae, af2checkpoint):
             print(f"Saving pae file without duplicates to {af2pae} \n")
             pae_no_duplicates.insert(0, 'pae', 'pae:')
             pae_no_duplicates.insert(2, 'tag', 'tag:')
+            pae_no_duplicates[3] = pae_no_duplicates[3].str.strip()
             pae_no_duplicates.to_csv(af2pae, sep=' ', index=False, header=False, quotechar=' ', quoting=3, escapechar=' ')
             print(f"Removing duplicates from the checkpoint file {af2checkpoint} \n")
             clean_checkpoint(pae_no_duplicates[2], af2checkpoint)
     else:
         print("No duplicates found in the PAE file. \n")
         pae_no_duplicates = pae
+        pae_no_duplicates[2] = pae_no_duplicates[2].str.strip()
+
     
     return pae_no_duplicates
 
@@ -194,7 +180,6 @@ def clean_silent(af2silent, af2scores_no_duplicates, pae_no_duplicates, af2check
             print("Removing duplicates from silent file \n")
             subprocess.run(f"silentdropduplicatesequences {silent_file} > {silent_file + '.unique'}", shell=True, cwd=silent_path)
             subprocess.run(f"mv {silent_file + '.unique'} {silent_file}", shell=True, cwd=silent_path)
-            subprocess.run(f"mv {silent_file + '.unique.idx'} {silent_file + '.idx'}", shell=True, cwd=silent_path)
             result_unique = subprocess.run(["silentls", silent_file], capture_output=True, cwd=silent_path)
             if result_unique.returncode == 0:
                 output_unique = result_unique.stdout.decode('utf-8')
@@ -247,24 +232,153 @@ def clean_silent(af2silent, af2scores_no_duplicates, pae_no_duplicates, af2check
     else:
         print("Error executing silentls command:", result.stderr.decode('utf-8'))
 
+def clean_af2results(af2score, af2pae, sc_out, pae_out, af2checkpoint):
+    """
+    Cleans the AF2 initial guess score and PAE files by removing identifiers that are not found in the checkpoint file.
+
+    Parameters:
+    - af2score (pd.DataFrame): DataFrame containing the AF2 score file.
+    - af2pae (pd.DataFrame): DataFrame containing the AF2 PAE file.
+    - sc_out (str): Path to the cleaned af2.sc output score file.
+    - pae_out (str): Path to the cleaned af2.pae output file.
+    - af2checkpoint (str): Path to the checkpoint file.
+
+    Returns:
+    - Writes the cleaned score file to the same path as the input score file.
+    - Writes the cleaned PAE file to the same path as the input PAE file.
+    """
+
+    print(f"Read in the checkpoint file {af2checkpoint} \n")
+    checkpoint = pd.read_csv(af2checkpoint, header=None, names=['file'])
+    if checkpoint['file'].str.contains('_af2pred').any():
+        checkpoint['file'] = checkpoint['file'].str.replace('_af2pred', '')
+    ids = checkpoint['file']
+    if af2score['description'].str.contains('_af2pred').any():
+        af2score['description'] = af2score['description'].str.replace('_af2pred', '')
+    if af2pae[2].str.contains('_af2pred').any():
+        af2pae[2] = af2pae[2].str.replace('_af2pred', '')
+        af2pae[2] = af2pae[2].str.strip()   
+    missing_ids = pd.concat([af2score['description'], af2pae[2]]).drop_duplicates().isin(ids)
+    if (~missing_ids).any():
+        print(f"Removing {(~missing_ids).sum()} missing id(s) from score and pae files \n")
+        af2score_cleaned = af2score[af2score['description'].isin(ids)]
+        af2pae_cleaned = af2pae[af2pae[2].isin(ids)]
+        print(f"Writing cleaned af2.sc file to {sc_out} \n")
+        af2score_cleaned.to_csv(sc_out, sep="\t", index=False)
+        print(f"Writing cleaned af2.pae file to {pae_out} \n")
+        af2pae_cleaned.insert(2, 'tag', 'tag:')
+        af2pae_cleaned.to_csv(pae_out, sep=' ', index=False, header=False, quotechar=' ', quoting=3, escapechar=' ')
+    elif missing_ids.all():
+        print("All ids in score and pae files are in the checkpoint file. \n")
+
+
 if __name__ == '__main__':
+    #################################
+    # Parse Arguments
+    #################################
+
+    parser = argparse.ArgumentParser()
+
+    # I/O Arguments
+    parser.add_argument( "-score", type=str, default=None, help='The path of a file of af2-initial guess scores' )
+    parser.add_argument( "-pae", type=str, default=None, help='The path of a file of af2-initial guess pae values' )
+    parser.add_argument( "-silent", type=str, default=None, help='The path of the af2-initial guess silent file' )
+    parser.add_argument( "-checkpoint", type=str, default=None, help='The path of af2-initial guess checkpoint file which will be overwritten' )
+    parser.add_argument( "-reference", type=str, default="pae_description", help='Cleaning checkpoint file referencing to the missing pae (`pae_description`) or missing score file (`description`)' )
+
+    args = parser.parse_args()
+
+    #################################
+
+    # Find default files
+    if args.score is None:
+        matching_sc = [f for f in os.listdir() if f.endswith("af2.sc")]
+        if len(matching_sc) == 1:
+            args.score = matching_sc[0]
+            print(f"\nDefault score file found: {args.score} \n")
+            continue_sc = input("Continue with default score file? (y/n): \n")
+            if continue_sc.lower() == 'n':
+                exit()
+            elif continue_sc.lower() == 'y':
+                pass
+        elif len(matching_sc) > 1:
+            print("Multiple score files found. Please specify the score file. Exiting. \n")
+            exit()
+        else:
+            print("No score file found. Please specify the score file. Exiting. \n")
+            exit()
+    if args.pae is None:
+        matching_pae = [f for f in os.listdir() if f.endswith("af2.pae")]
+        if len(matching_pae) == 1:
+            args.pae = matching_pae[0]
+            print(f"\nDefault pae file found: {args.pae} \n")
+            continue_pae = input("Continue with default PAE file? (y/n): \n")
+            if continue_pae.lower() == 'n':
+                exit()
+            elif continue_pae.lower() == 'y':
+                pass
+        elif len(matching_pae) > 1:
+            print("Multiple PAE files found. Please specify the PAE file. Exiting. \n")
+            exit()
+        else:
+            print("No PAE file found. Please specify the PAE file. Exiting. \n")
+            exit()
+    if args.silent is None:
+        matching_silent = [f for f in os.listdir() if f.endswith("af2.silent")]
+        if len(matching_silent) == 1:
+            args.silent = matching_silent[0]
+            print(f"\nDefault silent file found: {args.silent} \n")
+            continue_silent = input("Continue with default silent file? (y/n): \n")
+            if continue_silent.lower() == 'n':
+                exit()
+            elif continue_silent.lower() == 'y':
+                pass
+        elif len(matching_silent) > 1:
+            print("Multiple silent files found. Please specify the silent file. Exiting. \n")
+            exit()
+        else:
+            print("No silent file found. Please specify the silent file. Exiting. \n")
+            exit()
+    if args.checkpoint is None:
+        matching_checkpoint = [f for f in os.listdir() if f.endswith("af2_check.point")]
+        if len(matching_checkpoint) == 1:
+            args.checkpoint = matching_checkpoint[0]
+            print(f"\nDefault checkpoint file found: {args.checkpoint} \n")
+            continue_checkpoint = input("Continue with default checkpoint file? (y/n): \n")
+            if continue_checkpoint.lower() == 'n':
+                exit()
+            elif continue_checkpoint.lower() == 'y':
+                pass
+        elif len(matching_checkpoint) > 1:
+            print("Multiple checkpoint files found. Please specify the checkpoint file. Exiting. \n")
+            exit()
+        else:
+            print("No checkpoint file found. Please specify the checkpoint file. Exiting. \n")
+            exit()
+
     print('\n########################################################')
     print("### Cleaning duplicates from AF2 initial guess files ###")
     print('######################################################## \n')
     
-    af2scores_no_duplicates = clean_af2score(args.score, args.checkpoint, args.reference)
+    af2scores_no_duplicates = duplicates_af2score(args.score, args.checkpoint, args.reference)
     
     print('\n#########################################')
     print("### Cleaning duplicates from PAE file ###")
     print('######################################### \n')
 
-    pae_no_duplicates = clean_pae(args.pae, args.checkpoint)
-    
+    pae_no_duplicates = duplicates_pae(args.pae, args.checkpoint)
+        
     print('\n############################################')
     print("### Cleaning duplicates from silent file ###")
     print('############################################ \n')
     
     clean_silent(args.silent, af2scores_no_duplicates, pae_no_duplicates, args.checkpoint)
+
+    print('\n#################################################################################')
+    print("### Cleaning identifiers in AF2 score and PAE file but not in checkpoint file ###")
+    print('################################################################################# \n')
+    
+    clean_af2results(af2score = af2scores_no_duplicates, af2pae = pae_no_duplicates, sc_out = args.score, pae_out = args.pae, af2checkpoint=args.checkpoint)
 
 
     
